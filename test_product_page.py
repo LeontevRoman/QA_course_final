fb046e7c3a82de23ca812cc8155b156fd37bcf69
Roman Leontev <roman.l.v@yandex.ru>
2022-05-26 16:24:47 +0300

    final change

--- test_product_page.py
+++ test_product_page.py
@@ -6,7 +6,6 @@ import time
 from random import randint
 
 
-
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -22,9 +21,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-
     page.title_book_in_cards()
-
     page.price_book_in_cards()
 
 
